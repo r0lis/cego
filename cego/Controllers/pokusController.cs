5bb04934adced90ca5e0178ed0b248719e74c686
Rolis <luky.rolenec@seznam.cz>
2021-02-24 10:30:09 +0100

    novy

---
+++ cego/Controllers/pokusController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cego.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class pokusController : ControllerBase
+    {
+        [HttpGet("pokus/abc")]
+        public IActionResult abc(string jmeno ,string prijmeni)
+        {
+            string obracene = prijmeni + jmeno;
+            return Unauthorized(obracene);
+        }
+        [HttpGet("GetVal")]
+
+        public IActionResult GetVal(int cislo1, int cislo2)
+        {
+            if (cislo1 + cislo2 < 1) {
+                Console.WriteLine("Nulou nelze delit");
+            }
+
+            int vysledek = cislo1 / cislo2;
+            return Unauthorized(vysledek);
+        }
+        [HttpGet("GetVal2")]
+        public IActionResult GetVal2(int cislo = 20, int cislo1 = 2)
+        {
+            int vypocet = cislo / cislo1;
+            return NotFound(vypocet);
+        }
+    }
+}
---
+++ cego/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cego
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //Používá exception page, která ti øekne jak moc velkej jsi kokot
+            app.UseDeveloperExceptionPage();
+
+            //Používá swagger pouze pokud je development mode
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
